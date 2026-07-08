dab0bf71c5d0290711941458aae04e12ac8721e8
Gabanterli <gabriellarabanterli@gmail.com>
2026-07-07 21:54:54 -0300

    Create Alpha world entry UI shell

---
+++ scripts/AlphaWorldEntryController.cs
@@ -0,0 +1,9 @@
+﻿using Godot;
+
+public partial class AlphaWorldEntryController : Control
+{
+    public override void _Ready()
+    {
+        GD.Print("AlphaWorldEntryController loaded: UI shell only. Backend wiring pending.");
+    }
+}
